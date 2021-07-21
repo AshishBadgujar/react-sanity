c8307e177d5c63da0af08283bbadee525690c338
ashish <ashishbadgujar124@gmail.com>
2021-07-21 11:24:59 +0530

    New sanity

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 import sanityClient from "@sanity/client";
 
 export default sanityClient({
-  projectId: "yea2w5c3", // find this at manage.sanity.io, run `sanity init` to initialize
+  projectId: "rp3lgz7n", // find this at manage.sanity.io, run `sanity init` to initialize
   dataset: "production", // this is from when we answered those question from 'sanity init'
   useCdn: true,
 });
